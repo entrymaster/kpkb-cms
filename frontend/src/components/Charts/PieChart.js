750776db6f38c5a9f9ac568bbed2eeb6fcb5dd12
abhishekkh1701 <abhi17jan2004@gmail.com>
2024-03-16 12:19:06 +0530

    Report graphs

--- frontend/src/components/Charts/PieChart.js
+++ frontend/src/components/Charts/PieChart.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
-function PieChart({ chartData }) {
-  return <Pie data={chartData} />;
+function PieChart({ Data }) {
+  return <Pie data={Data} />;
 }
 
 export default PieChart;
