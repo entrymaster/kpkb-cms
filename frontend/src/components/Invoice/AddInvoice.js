ba36b8e77d355d65ebde9e2b44ed4546f4e6ec9e
Dhruv <dhruvgupta22@iitk.ac.in>
2024-03-28 16:11:14 +0530

    window reload removed for testing

--- frontend/src/components/Invoice/AddInvoice.js
+++ frontend/src/components/Invoice/AddInvoice.js
@@ -226,7 +226,7 @@ const AddNewInvoice = () => {
       console.error('Error generating bill:', error);
     } finally {
       setShowLoading(false);
-      window.location.reload(); 
+      // window.location.reload(); 
     }
   };
   
